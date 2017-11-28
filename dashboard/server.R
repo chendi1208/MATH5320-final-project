00ab4488be3469d2119a78bf23e9740e0cbde9cb
tian-gao <tian.gao@columbia.edu>
2017-11-28 16:37:27 -0500

    Init shinydashboard framework

---
+++ dashboard/server.R
@@ -0,0 +1,18 @@
+# Dashboard Server
+library(ggplot2)
+library(reshape2)
+library(dygraphs)
+library(xts)
+
+
+# user defined modules
+source('../model/gbmES.R')
+
+
+# server function
+shinyServer(function(input, output) {
+    output$table <- renderTable({
+        df <- data.frame(x = rnorm(100))
+        df
+    })
+})
---
+++ dashboard/ui.R
@@ -0,0 +1,34 @@
+# Dashboard UI
+library(shinydashboard)
+
+
+shinyUI(dashboardPage(
+    skin = 'blue',
+
+    # # # # # header & navbar
+    dashboardHeader(
+        title = 'title',
+        tags$li(
+            class = 'dropdown',
+            tags$a(
+                href = 'mailto:xxx@columbia.edu, yyy@columbia.edu',
+                tags$img(src = 'mail.png', width = '18px')))
+    ),
+
+    # # # # # sidebar
+        
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem('About', icon = icon('question-circle'),
+                menuItem('What', icon = icon('caret-right'), tabName = 'about-what'),
+                menuItem('How', icon = icon('caret-right'), tabName = 'about-how'),
+                menuItem('Who', icon = icon('caret-right'), tabName = 'about-who',
+                    menuItem('What', icon = icon('caret-right'), tabName = 'about-what'),
+                    menuItem('What', icon = icon('caret-right'), tabName = 'about-what')))
+        )
+    ),
+
+    # # # # # main panel
+    
+    dashboardBody(),
+))
