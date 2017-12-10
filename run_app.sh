28d6b599a96bcd617bea449e19dcd916191f0bb4
tian-gao <tian.gao@columbia.edu>
2017-12-10 09:45:22 -0500

    Compatible bash file path

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,7 @@
-cd /Users/doris/Documents/Columbia/Courseworks/MATH\ 5320\ Financial\ Risk\ Management\ and\ Regulation/project
-rscript run_app.R
+#!/bin/bash
+if [ "$USER" == "Troy" ]
+then
+  cd /Users/Troy/Documents/R/MATH5320-final-project/
+else
+  cd /Users/doris/Documents/Columbia/Courseworks/MATH\ 5320\ Financial\ Risk\ Management\ and\ Regulation/project
+fi
